bb8bc16cbcc36da6beb6c8e02a38278040ab4660
Rory Hartong-Redden <roryhr@gmail.com>
2024-11-01 10:01:36 -0600

    Migration worked!

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
         "NAME": "geodjango",
-        "USER": "geo",
+        "USER": "rory",
     }
 }
 
@@ -124,3 +124,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+GDAL_LIBRARY_PATH = "/opt/homebrew/Cellar/gdal/3.9.3_2/lib/libgdal.dylib"
+GEOS_LIBRARY_PATH = '/opt/homebrew/Cellar/geos/3.13.0/lib/libgeos_c.dylib'  # Replace with your actual GEOS path
